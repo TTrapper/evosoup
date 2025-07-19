a047a2c653b076af6c9219bddc9f87729d8016c5
mike <mike.sk.traynor@gmail.com>
2025-07-19 16:07:03 -0300

    Remove registers, all operands and instructions are in the soup, and everything is relative to pointer position

--- vm/vm.go
+++ vm/vm.go
@@ -13,32 +13,24 @@ const (
 // --- Instruction Set Opcodes ---
 const (
 	NOOP byte = iota
-	SET
 	COPY
 	ADD
 	SUB
-	JUMP_REL_IF_ZERO
-	READ_REL
-	WRITE_REL
+	JUMP_REL_IF_LT_ZERO
 	NumOpcodes // This must be the last entry, it counts the number of opcodes
 )
 
 var OpcodeNames = [...]string{
 	"NOOP",
-	"SET",
 	"COPY",
 	"ADD",
 	"SUB",
-	"JUMP_REL_IF_ZERO",
-	"READ_REL",
-	"WRITE_REL",
+	"JUMP_REL_IF_LT_ZERO",
 }
 
 // IP represents an Instruction Pointer, our digital organism.
-// It holds its own registers and all the tools needed to spawn a child.
 type IP struct {
 	ID         int
-	Registers  [NumRegisters]int32
 	CurrentPtr int32 // Current instruction pointer in the soup
 	Steps      int64 // Number of steps executed
 	Soup       []int32
@@ -46,105 +38,91 @@ type IP struct {
 
 // SavableIP defines the data for an IP that can be saved in a snapshot.
 type SavableIP struct {
-	ID           int
-	Registers    [NumRegisters]int32
-	CurrentPtr   int32
-	Steps        int64
+	ID         int
+	CurrentPtr int32
+	Steps      int64
 }
 
 // Savable returns a serializable representation of the IP.
 func (ip *IP) Savable() SavableIP {
 	return SavableIP{
-		ID:           ip.ID,
-		CurrentPtr:   ip.CurrentPtr,
-		Registers:    ip.Registers,
-		Steps:        ip.Steps,
+		ID:         ip.ID,
+		CurrentPtr: ip.CurrentPtr,
+		Steps:      ip.Steps,
 	}
 }
 
 // NewIP creates a new, minimal instruction pointer.
-// The concurrency tools (Population, Wg, etc.) must be set by the main loop.
 func NewIP(id int, soup []int32, startPtr int32) *IP {
 	return &IP{
-		ID:               id,
-		Soup:             soup,
-		CurrentPtr:       startPtr,
+		ID:         id,
+		Soup:       soup,
+		CurrentPtr: startPtr,
 	}
 }
 
 // Step executes a single instruction from the soup.
 func (ip *IP) Step() {
-
 	soupLen := int32(len(ip.Soup))
 
 	// Jump to a random part of the soup
 	// NOTE make this based on a timer rather than steps to
 	// tie it to the parent universe (the physical machine)
-	if (ip.Steps % 10000 == 0 ){
+	if ip.Steps%100000 == 0 {
 		ip.CurrentPtr = int32(rand.Intn(len(ip.Soup)))
 	}
 
-	// The pointer should always be valid at the start of a step due to the wrap at the end.
-	// Helper function to safely read a value from the soup, handling pointer wrap.
-	safeReadSoup := func() int32 {
-		val := ip.Soup[(ip.CurrentPtr%soupLen+soupLen)%soupLen]
+	// --- Helper Functions ---
+	// Wraps an address to the valid range of the soup.
+	wrapAddr := func(addr int32) int32 {
+		return (addr%soupLen + soupLen) % soupLen
+	}
+
+	// Reads the value at the CurrentPtr and increments the pointer.
+	safeRead := func() int32 {
+		val := ip.Soup[wrapAddr(ip.CurrentPtr)]
 		ip.CurrentPtr++
 		return val
 	}
 
-	opcode := (safeReadSoup()%int32(NumOpcodes) + int32(NumOpcodes)) % int32(NumOpcodes)
-	originalPtr := ip.CurrentPtr
-
-	// Helper function to safely read a register index from the soup.
-	safeRegIndex := func() int32 {
-		val := safeReadSoup()
-		return (val%NumRegisters + NumRegisters) % NumRegisters
+	// Reads an offset value and returns the final, absolute, wrapped address
+	// calculated relative to the location of the offset value itself.
+	getRelAddr := func() int32 {
+		currentAddr := wrapAddr(ip.CurrentPtr)
+		offsetVal := safeRead()
+		return wrapAddr(currentAddr + offsetVal)
 	}
 
-	// --- Instruction Decoder ---
+	// --- Instruction Execution ---
+	opcodeAddr := wrapAddr(ip.CurrentPtr)
+	opcode := (safeRead() % int32(NumOpcodes) + int32(NumOpcodes)) % int32(NumOpcodes)
+
 	switch byte(opcode) {
 	case NOOP:
-	// PC already incremented
-	case SET:
-		regDest := safeRegIndex()
-		value := safeReadSoup()
-		ip.Registers[regDest] = value
+		// PC was already incremented by safeRead.
 	case COPY:
-		regDest := safeRegIndex()
-		regSrc := safeRegIndex()
-		ip.Registers[regDest] = ip.Registers[regSrc]
+		destAddr := getRelAddr()
+		srcAddr := getRelAddr()
+		ip.Soup[destAddr] = ip.Soup[srcAddr]
 	case ADD:
-		regDest := safeRegIndex()
-		regSrc1 := safeRegIndex()
-		regSrc2 := safeRegIndex()
-		ip.Registers[regDest] = ip.Registers[regSrc1] + ip.Registers[regSrc2]
+		destAddr := getRelAddr()
+		src1Addr := getRelAddr()
+		src2Addr := getRelAddr()
+		ip.Soup[destAddr] = ip.Soup[src1Addr] + ip.Soup[src2Addr]
 	case SUB:
-		regDest := safeRegIndex()
-		regSrc1 := safeRegIndex()
-		regSrc2 := safeRegIndex()
-		ip.Registers[regDest] = ip.Registers[regSrc1] - ip.Registers[regSrc2]
-	case JUMP_REL_IF_ZERO:
-		regCond := safeRegIndex()
-		regOffset := safeRegIndex() // Always read both arguments
-		if ip.Registers[regCond] == 0 {
-			// Jump relative to the current pointer (which is already past the arguments)
-			ip.CurrentPtr += ip.Registers[regOffset]
+		destAddr := getRelAddr()
+		src1Addr := getRelAddr()
+		src2Addr := getRelAddr()
+		ip.Soup[destAddr] = ip.Soup[src1Addr] - ip.Soup[src2Addr]
+	case JUMP_REL_IF_LT_ZERO:
+		condAddr := getRelAddr()
+		jumpOffset := safeRead()
+		if ip.Soup[condAddr] < 0 {
+			// The jump is relative to the JUMP instruction's location (opcodeAddr),
+			// not the location of the offset operand.
+			ip.CurrentPtr = opcodeAddr + jumpOffset
 		}
-	case READ_REL:
-		regDest := safeRegIndex()
-		regOffset := safeRegIndex()
-		readAddr := originalPtr + ip.Registers[regOffset]
-		readAddr = (readAddr%soupLen + soupLen) % soupLen // Wrap address
-		ip.Registers[regDest] = ip.Soup[readAddr]
-	case WRITE_REL:
-		regSrc := safeRegIndex()
-		regOffset := safeRegIndex()
-		writeAddr := originalPtr + ip.Registers[regOffset]
-		writeAddr = (writeAddr%soupLen + soupLen) % soupLen // Wrap address
-		ip.Soup[writeAddr] = ip.Registers[regSrc]
 	}
 
-	// Wrap the final pointer to ensure it's always valid.
-	ip.CurrentPtr = (ip.CurrentPtr%soupLen + soupLen) % soupLen
 	ip.Steps++
 }
